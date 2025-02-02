e2706fd34967356d7c8f7bf02db352b9b418ade4
Pavol Ondrušek <pavol.ondrusek@gmail.com>
2025-02-02 22:15:48 +0100

    [4.1.3] Extended struct Grid with another usefull methods (neighbors, fill, etc.)

--- src/grids/point.rs
+++ src/grids/point.rs
@@ -11,7 +11,7 @@ impl Point {
         Self { x, y }
     }
 
-    pub fn neighbor(&self, direction: Direction) -> Self {
+    pub fn neighbor(&self, direction: &Direction) -> Self {
         match direction {
             Direction::North => Self::new(self.x, self.y - 1),
             Direction::NorthEast => Self::new(self.x + 1, self.y - 1),
@@ -33,13 +33,13 @@ mod tests {
     fn test_neighbor() {
         let point = Point { x: 5, y: 3 };
 
-        assert_eq!(point.neighbor(Direction::North), Point { x: 5, y: 2 });
-        assert_eq!(point.neighbor(Direction::NorthEast), Point { x: 6, y: 2 });
-        assert_eq!(point.neighbor(Direction::East), Point { x: 6, y: 3 });
-        assert_eq!(point.neighbor(Direction::SouthEast), Point { x: 6, y: 4 });
-        assert_eq!(point.neighbor(Direction::South), Point { x: 5, y: 4 });
-        assert_eq!(point.neighbor(Direction::SouthWest), Point { x: 4, y: 4 });
-        assert_eq!(point.neighbor(Direction::West), Point { x: 4, y: 3 });
-        assert_eq!(point.neighbor(Direction::NorthWest), Point { x: 4, y: 2 });
+        assert_eq!(point.neighbor(&Direction::North), Point { x: 5, y: 2 });
+        assert_eq!(point.neighbor(&Direction::NorthEast), Point { x: 6, y: 2 });
+        assert_eq!(point.neighbor(&Direction::East), Point { x: 6, y: 3 });
+        assert_eq!(point.neighbor(&Direction::SouthEast), Point { x: 6, y: 4 });
+        assert_eq!(point.neighbor(&Direction::South), Point { x: 5, y: 4 });
+        assert_eq!(point.neighbor(&Direction::SouthWest), Point { x: 4, y: 4 });
+        assert_eq!(point.neighbor(&Direction::West), Point { x: 4, y: 3 });
+        assert_eq!(point.neighbor(&Direction::NorthWest), Point { x: 4, y: 2 });
     }
 }
